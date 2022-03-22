75a8beffe3d1ea0782a046443cb98ef166ed141f
Steve McDougall <juststevemcd@gmail.com>
2022-03-22 19:53:39 +0000

    Passing in an argument and handling none passed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{env, process::exit};
+
 fn main() {
+    let mut args: env::Args = env::args();
+
+    let handle: String = match args.nth(1) {
+        Some(h) => h,
+        None => {
+            println!("Please provide a GitHub username.");
+            exit(1);
+        }
+    };
+
+    dbg!(handle);
+
     println!("Hello, world!");
 }
